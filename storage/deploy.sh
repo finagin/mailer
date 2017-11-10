469123488d47fb9e1c17c23a6a20a30ce51c390c
finagin <Igor@Finag.in>
2017-11-10 16:40:39 +0300

    HotFix Travis-CI Deploy

--- storage/deploy.sh
+++ storage/deploy.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-composer global require "laravel/envoy=~1.0";
-
-if [ $(! echo "$PATH" | grep -q "~/.composer/vendor/bin") ]; then
-	export PATH=$PATH:~/.composer/vendor/bin;
-fi;
-
-export TRAVIS_BUILD_DIR;
-cd $TRAVIS_BUILD_DIR;
-envoy run deploy;
+echo "SUCCESS!";
